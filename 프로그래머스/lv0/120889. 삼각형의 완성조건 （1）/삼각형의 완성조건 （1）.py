ab1949a33a74727107aa1da32469374e42a6110c
clghdi98 <77911943+chiho98@users.noreply.github.com>
2023-05-05 23:04:07 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
@@ -1,6 +1,3 @@
 def solution(sides):
         sides.sort(reverse=True)
-        if sides[0] >= (sides[1] + sides[2]):
-            return 2
-        elif sides[0] < (sides[1] + sides[2]):
-            return 1
+        return 1 if sides[0] < sides[1] + sides[2] else 2
