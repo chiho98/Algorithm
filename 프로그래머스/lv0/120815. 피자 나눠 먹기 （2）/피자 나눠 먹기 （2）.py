349bf41b879285e6dfa89d9ccae4c54a355cb744
clghdi98 <77911943+chiho98@users.noreply.github.com>
2023-05-11 23:59:30 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    for i in range(1,n+1):
+        if 6*i % n == 0:
+            answer += 1
+            break
+        else:
+            answer += 1
+            continue
+    return answer
