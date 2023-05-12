1e13e28abb57336dba1b5f6edb447d8ef37e8bb6
clghdi98 <77911943+chiho98@users.noreply.github.com>
2023-05-12 21:58:38 +0900

    [level 0] Title: 369게임, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.py
@@ -0,0 +1,8 @@
+def solution(order):
+    answer = 0
+    order = str(order)
+    order = list(order)
+    for i in order:
+        if i == "3" or i == "6" or i == "9":
+            answer += 1
+    return answer
