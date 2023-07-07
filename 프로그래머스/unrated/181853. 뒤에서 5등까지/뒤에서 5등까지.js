624eb915cff36dcfdd522283366a93057604144d
clghdi98 <77911943+chiho98@users.noreply.github.com>
2023-07-07 17:28:42 +0900

    [unrated] Title: 뒤에서 5등까지, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.js
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.js
@@ -1,8 +1,4 @@
 function solution(num_list) {
     const arr = num_list.sort((a,b) => a - b)
-    const answer = []
-    for(let i = 0 ; i <= 4; i++) {
-        answer.push(num_list[i])
-    }
-    return answer
+    return arr.slice(0,5)
 }
